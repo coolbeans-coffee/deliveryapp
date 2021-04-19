ac3c91b668644c3020bdc7d2a5f9097ac689910a
coolbeans <coolbeans.team.th@gmail.com>
2021-04-19 22:08:48 +0700

    update menu.html

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/') # URL Route
 def home(): # Build a function called "home"
-    return render_template('home.html') # Return strings to a desired url
+    return render_template('index.html') # Return strings to a desired url
 
 @app.route('/beans') # URL Route
 def beans(): # Build a function called "home"
